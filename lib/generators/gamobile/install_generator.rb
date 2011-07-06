93ab28772fbf25914f9d86b53631a02aae2964c2
terut <terut.dev+github@gmail.com>
2011-07-06 19:56:13 +0900

    edit gamobile:install

--- lib/generators/gamobile/install_generator.rb
+++ lib/generators/gamobile/install_generator.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 module Gamobile
   module Generators
-    class GamobileGenerator < Rails::Generators::NamedBase
+    class InstallGenerator < Rails::Generators::NamedBase
       source_root File.expand_path('../templates', __FILE__)
       argument :tracker_code, :type => :string, :default => 'xx-xxxxxxx-xx'
 
--- lib/generators/gamobile/templates/gamobile_helper.rb
+++ lib/generators/gamobile/templates/gamobile_helper.rb
@@ -2,12 +2,16 @@
 module <%= class_name %>Helper
   # mobile用のanalyticsタグ
   def ga_url
-    return mobile_ga_path(
-      :utmac => <%= tracker_code %>,
+    options = {
+      :utmac => "<%= tracker_code %>",
       :utmn => rand(0x7fffffff),
       :utmr => request.referer.blank? ? '-' : u(request.referer),
       :utmp => u(request.fullpath),
       :guid => 'ON'
-    )
+    }
+    # TODO Rewrite url adapted to your routes.rb
+    # Example:
+    #   return ga_url(options)
+    return "#"
   end
 end
